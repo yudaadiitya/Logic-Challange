cd69d150959d53b9cd6ec6f5a49dbd01aa38673d
Yuda <yuda10aditya@gmail.com>
2021-02-16 23:07:56 +0700

    challenge 9

--- challange9.js
+++ challange9.js
@@ -13,27 +13,27 @@ function spiral(param1) {
     var maxCol = param1 - 1;
     var minRow = 0;
     var maxRow = param1 - 1;
-    var result = [];
+    var hasil = [];
 
-    while (result.length < angka) {
+    while (hasil.length < angka) {
         for (let x = minRow; x <= maxRow; x++) {
-            result.push(matriks[minCol][x]);
+            hasil.push(matriks[minCol][x]);
         }
         minCol++;
         for (let y = minCol; y <= maxCol; y++) {
-            result.push(matriks[y][maxRow]);
+            hasil.push(matriks[y][maxRow]);
          }
         maxRow--;
         for (let x = maxRow; x >= minRow; x--) {
-            result.push(matriks[maxCol][x]);
+            hasil.push(matriks[maxCol][x]);
         }
         maxCol--;
         for (let y = maxCol; y >= minCol; y--) {
-            result.push(matriks[y][minRow]);
+            hasil.push(matriks[y][minRow]);
         }    
         minRow++;
     }
-    console.log(result);
+    console.log(hasil);
 } 
 
 spiral(5);
